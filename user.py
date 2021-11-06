f6955e9abac535eee46cdd916ee9cad7086f3c28
shawn <shawn@kali.Hacker>
2021-11-06 07:12:00 -0400

    initial

--- user.py
+++ user.py
@@ -51,10 +51,10 @@ class User:
 
 
 
-    def __init__(self,first_name,last_name,phone_number,email,passwordname):
+    def __init__(self,first_name,last_name,phone_number,email,password):
 
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
-        self.password = passwordname
+        self.password = password
